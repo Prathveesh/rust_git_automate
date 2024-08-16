35b38b00ef7f6c2ab606f94359a7a0a42530dfd2
Prathveesh <prathveesh240301@gmail.com>
2024-08-16 20:17:55 +0530

    wise-rings

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+use std::process::{Command, exit};
+use names::Generator;
+
+fn update_commit_push() {
+    let add_command = Command::new("git")
+        .arg("add")
+        .arg("-A")
+        .output()
+        .expect("Failed to execute git add command");
+
+    if !add_command.status.success() {
+        eprintln!("Error: Fail in adding files to the local repo.");
+        exit(1);
+    }
+
+    let commit_command = Command::new("git")
+        .arg("commit")
+        .arg("-m")
+        .arg(name_gen())
+        .output()
+        .expect("Failed to execute git commit command");
+
+    if !commit_command.status.success() {
+        eprintln!("Error: Fail in committing changes.");
+        exit(1);
+    }
+
+    let push_command = Command::new("git")
+        .arg("push")
+        .arg("origin")
+        .arg("master")
+        .output()
+        .expect("Failed to execute git push command");
+
+    if !push_command.status.success() {
+        eprintln!("Error: Fail in pushing changes to remote.");
+        exit(1);
+    }
+
+    println!("Changes are successfully added, committed, and pushed.");
+}
+fn name_gen() -> String{
+let mut generator = Generator::default();
+    generator.next().unwrap()
+}
+fn main() {
+    update_commit_push();
+}
